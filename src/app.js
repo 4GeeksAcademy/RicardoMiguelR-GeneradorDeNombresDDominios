41b5efc6274fe137bcdda2de69cc0a239a8df079
Ricardo Miguel R <ricardomr.23@hotmail.com>
2024-02-23 21:27:32 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Ricardo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + ".com");
+        console.log(adj[j] + ".us");
+        console.log(noun[k] + ".net");
+      }
+    }
+  }
 };
